66d9c47e26c10a2b0481eba3e572fad85a3391dc
fshooter <fshooter@gmail.com>
2015-07-14 14:34:44 +0800

    bugfix

--- init.el
+++ init.el
@@ -77,81 +77,29 @@
 (add-to-list 'process-coding-system-alist '("[gG][iI][tT]" utf-8 . utf-8))
 (add-to-list 'file-coding-system-alist '(".*\\.git\\.*" utf-8 . utf-8))
 
-(defconst microsoft-c-style
-  `((c-recognize-knr-p . nil)
-    (c-enable-xemacs-performance-kludge-p . t) ; speed up indentation in XEmacs
-    (c-basic-offset . 4)
-    (indent-tabs-mode . nil)
-    (c-comment-only-line-offset . 0)
-    (c-hanging-braces-alist . ((defun-open after)
-                               (defun-close before after)
-                               (class-open after)
-                               (class-close before after)
-                               (inexpr-class-open after)
-                               (inexpr-class-close before)
-                               (namespace-open after)
-                               (inline-open after)
-                               (inline-close before after)
-                               (block-open after)
-                               (block-close . c-snug-do-while)
-                               (extern-lang-open after)
-                               (extern-lang-close after)
-                               (statement-case-open after)
-                               (substatement-open after)))
-    (c-hanging-colons-alist . ((case-label)
-                               (label after)
-                               (access-label after)
-                               (member-init-intro before)
-                               (inher-intro)))
-    (c-hanging-semi&comma-criteria
-     . (c-semi&comma-no-newlines-for-oneline-inliners
-        c-semi&comma-inside-parenlist
-        c-semi&comma-no-newlines-before-nonblanks))
-    (c-indent-comments-syntactically-p . t)
-    (comment-column . 40)
-    (c-indent-comment-alist . ((other . (space . 2))))
-    (c-cleanup-list . (brace-else-brace
-                       brace-elseif-brace
-                       brace-catch-brace
-                       empty-defun-braces
-                       defun-close-semi
-                       list-close-comma
-                       scope-operator))
-    (c-offsets-alist . ((arglist-intro . +)
-                        (func-decl-cont . ++)
-                        (member-init-intro . ++)
-                        (inher-intro . ++)
-                        (comment-intro . 0)
-                        (arglist-close . c-lineup-arglist)
-                        (topmost-intro . 0)
-                        (block-open . 0)
-                        (inline-open . 0)
-                        (substatement-open . 0)
-                        (statement-cont
-                         .
-                         (,(when (fboundp 'c-no-indent-after-java-annotations)
-                             'c-no-indent-after-java-annotations)
-                          ,(when (fboundp 'c-lineup-assignments)
-                             'c-lineup-assignments)
-                          ++))
-                        (label . /)
-                        (case-label . 0)
-                        (statement-case-open . 0)
-                        (statement-case-intro . +) ; case w/o {
-                        (access-label . /)
-                        (innamespace . 0))))
-  "Microsoft C/C++ Programming Style.")
-
-(defun microsoft-set-c-style ()
+(defun my-set-c-style ()
   "Set the current buffer's c-style to Microsoft C/C++ Programming
   Style. Meant to be added to `c-mode-common-hook'."
   (interactive)
   (make-local-variable 'c-tab-always-indent)
   (setq c-tab-always-indent t)
-  (c-add-style "Microsoft" microsoft-c-style t))
+  (c-add-style "Google" google-c-style)
+  (c-add-style "MyCodeStyle"
+			   '("Google"
+				 (c-basic-offset . 4)
+				 (c-offsets-alist . (
+									 (func-decl-cont . +)
+									 (member-init-intro . +)
+									 (inher-intro . +)
+									 (statement-case-open . 0)
+									 (case-label . 0))))
+			   t)
+  (define-key c-mode-base-map "\C-m" 'newline-and-indent)
+  (define-key c-mode-base-map [ret] 'newline-and-indent))
 
-(add-hook 'c-mode-common-hook 'google-set-c-style)
-(add-hook 'c-mode-common-hook 'google-make-newline-indent)
+(add-hook 'c-mode-common-hook 'my-set-c-style)
+
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
 
 ;; white space mode options
 (setq whitespace-style '(face lines-tail trailing)
